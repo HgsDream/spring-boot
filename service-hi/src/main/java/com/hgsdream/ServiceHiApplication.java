c2f2c3caf5af16db9c2a025e0a62d13c25cdb8bd
Hong-PC\Hong <962284660@qq.com>
2017-07-04 21:24:52 +0800

    提交项目内容

---
+++ service-hi/src/main/java/com/hgsdream/ServiceHiApplication.java
@@ -0,0 +1,26 @@
+package com.hgsdream;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class ServiceHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHiApplication.class, args);
+	}
+
+	@Value("${server.port}")
+	String port;
+	@RequestMapping(value = "/hi")
+	public String home(@RequestParam String name){
+		return "hi"+name+",i'm from:"+port;
+	}
+}
