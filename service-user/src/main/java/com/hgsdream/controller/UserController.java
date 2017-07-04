4a50dc921f6a1b8ec9a624f96addc28720af9720
Hong-PC\Hong <962284660@qq.com>
2017-07-04 22:57:07 +0800

    增加注册方法

--- service-user/src/main/java/com/hgsdream/controller/UserController.java
+++ service-user/src/main/java/com/hgsdream/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.hgsdream.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +20,10 @@ public class UserController {
     @RequestMapping(value = "/infor")
     public String userInfo(@RequestParam String info){
         return "the info ="+info;
+    }
+
+    @PostMapping(value = "register")
+    public String register(){
+         return "1";
     }
 }
